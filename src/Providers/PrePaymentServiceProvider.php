74e30feb6be31ccec4c4bdc8f1d2c4697f5651a2
Jens Konopka <jens.konopka@plentymarkets.com>
2016-12-14 10:54:12 +0100

    remove unused event listener

--- src/Providers/PrePaymentServiceProvider.php
+++ src/Providers/PrePaymentServiceProvider.php
@@ -3,18 +3,10 @@
 namespace PrePayment\Providers;
 
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Modules\Payment\Method\Contracts\PaymentMethodRepositoryContract;
-use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
-use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
-use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Modules\Payment\Method\Contracts\PaymentMethodContainer;
 use PrePayment\Methods\PrePaymentPaymentMethod;
-use PrePayment\Helper\PrePaymentHelper;
 use Plenty\Modules\Basket\Events\Basket\AfterBasketCreate;
 use Plenty\Modules\Basket\Events\Basket\AfterBasketChanged;
-use \Plenty\Modules\Payment\Events\Checkout\GetPaymentMethodContent;
-use Plenty\Modules\Payment\Events\Checkout\ExecutePayment;
-
 
 /**
  * Class PrePaymentServiceProvider
@@ -32,39 +24,12 @@ class PrePaymentServiceProvider extends ServiceProvider
       }
 
       /**
-       * @param Dispatcher                $eventDispatcher
-       * @param PrePaymentHelper          $prePaymentHelper
        * @param PaymentMethodContainer    $payContainer
        */
-      public function boot(Dispatcher $eventDispatcher, PrePaymentHelper $prePaymentHelper, PaymentMethodContainer $payContainer)
+      public function boot(PaymentMethodContainer $payContainer)
       {
             //Register the Pre Payment Plugin
             $payContainer->register('plenty_prepayment::PREPAYMENT', PrePaymentPaymentMethod::class,
                                     [AfterBasketChanged::class, AfterBasketCreate::class]   );
-
-            //Listen for the event to show bank data to the customer
-            /*$eventDispatcher->listen(GetPaymentMethodContent::class,
-                                      function(GetPaymentMethodContent $event) use( $prePaymentHelper)
-                                      {
-                                            if($event->getMop() == $prePaymentHelper->getPrePaymentMopId())
-                                            {
-                                                  //ToDo set Account Data to the container
-                                                  $event->setValue('Dummy Account Data');
-                                                  $event->setType( 'htmlContent');
-                                            }
-
-                                      }   );*/
-
-            //Listen for the event which executes the payment
-            $eventDispatcher->listen(ExecutePayment::class,
-                    function(ExecutePayment $event) use( $prePaymentHelper)
-                    {
-                          if($event->getMop() == $prePaymentHelper->getPrePaymentMopId())
-                          {
-                                $event->setValue('<h1>Vorkasse<h1>');
-                                $event->setType('htmlContent');
-                          }
-                    });
-
       }
 }
