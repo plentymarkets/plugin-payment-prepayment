ef314dfdcd863ec0e0d2211c5d644b022eec5959
Christian Kunze <christian.kunze@plentymarkets.com>
2017-02-28 11:48:20 +0100

    Middleware OAuth added to RouteServiceProvider

--- src/Providers/PrePaymentRouteServiceProvider.php
+++ src/Providers/PrePaymentRouteServiceProvider.php
@@ -10,6 +10,7 @@ namespace PrePayment\Providers;
 
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\Router;
+use Plenty\Plugin\Routing\ApiRouter;
 
 class PrePaymentRouteServiceProvider extends RouteServiceProvider
 {
@@ -17,10 +18,16 @@ class PrePaymentRouteServiceProvider extends RouteServiceProvider
     /**
      * @param Router $router
      */
-    public function map(Router $router)
+    public function map(Router $router , ApiRouter $apiRouter)
     {
-        $router->get('payment/prepayment/settings/{plentyId}'   , 'PrePayment\Controllers\SettingsController@loadSettings');
-        $router->put('payment/prepayment/settings'              , 'PrePayment\Controllers\SettingsController@saveSettings');
+        $apiRouter->version(['v1'], ['namespace' => 'PayPal\Controllers', 'middleware' => 'oauth'],
+            function (ApiRouter $apiRouter)
+            {
+                $apiRouter->get('payment/prepayment/settings/{plentyId}', 'PrePayment\Controllers\SettingsController@loadSettings');
+                $apiRouter->put('payment/prepayment/settings', 'PrePayment\Controllers\SettingsController@saveSettings');
+            });
+        $router->get('payment/prepayment/settings/{plentyId}', 'PrePayment\Controllers\SettingsController@loadSettings');
+        $router->put('payment/prepayment/settings', 'PrePayment\Controllers\SettingsController@saveSettings');
     }
 
 }
