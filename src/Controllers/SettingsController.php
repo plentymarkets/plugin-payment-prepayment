56f84f7554c04a3fd350dad9a0b86406c057435e
Christian Kunze <christian.kunze@plentymarkets.com>
2017-03-09 17:34:46 +0100

    RouteProvider cleaned up

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -16,11 +16,6 @@ use Plenty\Plugin\Http\Response;
 class SettingsController extends Controller
 {
 
-    public function loadSetting(Response $response, SettingsService $service, $plentyId, $setting, $lang)
-    {
-        return $response->json($service->getSetting($setting, $lang));
-    }
-
     public function loadSettings(Response $response, SettingsService $service, $plentyId, $lang)
     {
         return $response->json($service->getSettingsForPlentyId($plentyId, $lang));
