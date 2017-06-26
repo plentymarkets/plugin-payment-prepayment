e4822525e6be2eac3ea717a607274936cfc55b5b
David Bohla <david.bohla@plentymarkets.com>
2017-06-26 12:24:54 +0200

    UPDATE path

--- src/Methods/PrePaymentPaymentMethod.php
+++ src/Methods/PrePaymentPaymentMethod.php
@@ -110,7 +110,7 @@ class PrePaymentPaymentMethod extends PaymentMethodService
         elseif($this->settings->getSetting('logo') == 2)
         {
             $app = pluginApp(Application::class);
-                $icon = $app->getUrlPath('prepayment').'/images/logos/de-pp-logo.png';
+                $icon = $app->getUrlPath('prepayment').'/images/icon.png';
 
                 return $icon;
         }
