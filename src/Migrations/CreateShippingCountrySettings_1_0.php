ac1fb60cf4b5b1f1976efca711c03405b0ccbd89
Marcel Pönninghaus <>
2019-02-28 17:34:06 +0100

    shipping country migration split

--- src/Migrations/CreateShippingCountrySettings_1_0.php
+++ src/Migrations/CreateShippingCountrySettings_1_0.php
@@ -2,9 +2,6 @@
 
 namespace CashInAdvance\Migrations;
 
-use CashInAdvance\Models\Settings;
-use Plenty\Modules\Plugin\DataBase\Contracts\DataBase;
-use CashInAdvance\Services\SettingsService;
 use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
 use CashInAdvance\Models\ShippingCountrySettings;
 
@@ -13,55 +10,8 @@ class CreateShippingCountrySettings_1_0
 {
     use \Plenty\Plugin\Log\Loggable;
 
-    /** @var  DataBase */
-    private $db;
-
-    public function __construct(DataBase $db)
-    {
-        $this->db = $db;
-    }
-
     public function run(Migrate $migrate)
     {
-        try {
-            $migrate->createTable(ShippingCountrySettings::class);
-            $this->migrateShippingCountries();
-        } catch (\Exception $ex) {
-            $this->getLogger(__CLASS__.'::'.__FUNCTION__)->error('CashInAdvance', $ex);
-        }
-
+        $migrate->createTable(ShippingCountrySettings::class);
     }
-
-    private function migrateShippingCountries() {
-        try {
-            /** @var SettingsService $service */
-            $service = pluginApp(SettingsService::class);
-            $clients = $service->getClients();
-
-            foreach ($clients as $plentyId) {
-                /** @var Settings[] $storedShippingCountries */
-                $storedShippingCountries = $this->db->query(Settings::MODEL_NAMESPACE)
-                    ->where('plentyId', '=', $plentyId)
-                    ->where('name', '=', 'shippingCountries')->get();
-
-                $shippingCountriesString = '';
-                foreach ($storedShippingCountries as $shippingCountriesRow){
-                    $shippingCountriesString = $shippingCountriesRow->value;
-                }
-                $shippingCountriesArray = explode('-/-', $shippingCountriesString);
-                foreach ($shippingCountriesArray as $shippingCountry) {
-                    if(strlen($shippingCountry) && $shippingCountry>0) {
-                        /** @var ShippingCountrySettings $shippingCountrySettings */
-                        $shippingCountrySettings = pluginApp(ShippingCountrySettings::class);
-                        $shippingCountrySettings->plentyId = $plentyId;
-                        $shippingCountrySettings->shippingCountryId = $shippingCountry;
-                        $this->db->save($shippingCountrySettings);
-                    }
-                }
-            }
-        } catch(\Exception $ex) {
-            $this->getLogger(__CLASS__.'::'.__FUNCTION__)->error('CashInAdvance', $ex);
-        }
-    }
-
 }
