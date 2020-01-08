2dc17faed7b07daf2cabfd5917eb10baf27bea37
Kevin Fink <kevin.fink@plentymarkets.com>
2020-01-08 09:07:00 +0100

    added argument type for getBackendName

--- src/Methods/CashInAdvancePaymentMethod.php
+++ src/Methods/CashInAdvancePaymentMethod.php
@@ -203,10 +203,10 @@ class CashInAdvancePaymentMethod extends PaymentMethodService
     /**
      * Get the name for the backend
      *
-     * @param $lang
+     * @param string $lang
      * @return string
      */
-    public function getBackendName($lang):string
+    public function getBackendName(string $lang):string
     {
         return $this->getName($lang);
     }
