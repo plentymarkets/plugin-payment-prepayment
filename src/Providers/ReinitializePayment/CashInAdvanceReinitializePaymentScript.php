ccae40e5733e3852931b7169bf839e87b3c03cf9
Raphael Adam <raphael.adam@plentymarkets.com>
2020-10-21 14:15:32 +0200

    use localization repository

--- src/Providers/ReinitializePayment/CashInAdvanceReinitializePaymentScript.php
+++ src/Providers/ReinitializePayment/CashInAdvanceReinitializePaymentScript.php
@@ -4,18 +4,18 @@ namespace CashInAdvance\Providers\ReinitializePayment;
 
 use Plenty\Plugin\Templates\Twig;
 use CashInAdvance\Helper\CashInAdvanceHelper;
-use CashInAdvance\Services\SessionStorageService;
+use Plenty\Modules\Webshop\Contracts\LocalizationRepositoryContract;
 
 class CashInAdvanceReinitializePaymentScript
 {
-	public function call(Twig $twig, SessionStorageService $sessionStorageService): string
+	public function call(Twig $twig, LocalizationRepositoryContract $localizationRepository): string
 	{
 		/** @var CashInAdvanceHelper $paymentHelper */
 		$paymentHelper = pluginApp(CashInAdvanceHelper::class);
 		$paymentId = $paymentHelper->getCashInAdvanceMopId();
 		return $twig->render('PrePayment::ReinitializePaymentScript', [
 		    'paymentMethodId' => $paymentId,
-            'lang' => $sessionStorageService->getLang()
+            'lang' => $localizationRepository->getLanguage()
         ]);
 	}
 }
